328c289b250824e1febda970f9c3f3de61e35860
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-03-04 15:04:00 +0100

    Add location permissions and weather animations

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
@@ -36,6 +37,31 @@ catch(e){
 }
 // weather animations
 
+String getWeatherAnimation(String? mainCondition) {
+  if (mainCondition == null) return '/sunny.json';
+
+  switch (mainCondition.toLowerCase()) {
+    case 'clouds':
+    case 'mist':
+    case 'smoke':
+    case 'haze':
+    case 'dust':
+    case 'fog':
+      return '/cloud.json';
+    case 'rain':
+    case 'drizzle':
+    case 'shower rain':
+      return '/rain.json';
+    case 'thunderstorm':
+      return '/thunder.json';
+    case 'clear':
+      return '/sunny.json';
+    default:
+      return '/sunny.json';
+  }
+}
+
+
 // init state
 @override
 void initState(){
@@ -48,13 +74,24 @@ void initState(){
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Column(
-        children: [
+      backgroundColor: Colors.grey[800],
+      body: Center(
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
           // city name
-          Text(_weather?.cityName ?? " Loading city..", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+          Text(_weather?.cityName ?? " Loading city.."),
+
+          // animation 
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
           // temperature
-          Text('${_weather?.temperature ?? 0}°C', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+          Text('${_weather?.temperature ?? 0}°C'),
+
+          // weather condition
+          Text(_weather?.mainCondition ?? ""),
         ],
+        ),
       ),
     );
   }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/models/weather_model.dart';
